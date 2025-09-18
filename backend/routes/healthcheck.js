37ee8339de9c48267751a4518d7c182b4e28f178
Bhusal-Ravi <ravidonbhusal@gmail.com>
2025-09-18 07:27:57 +0545

    .

--- backend/routes/healthcheck.js
+++ backend/routes/healthcheck.js
@@ -2,6 +2,7 @@ const express= require('express');
 const router= express.Router();
 
 router.get('/health', (req, res) => {   
+    console.log('Health check endpoint was called');
     res.status(200).json({ status: 'OK', message: 'Server is healthy' });
 });
 
